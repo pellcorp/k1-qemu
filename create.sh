544ad94afcc68e5daf75b2a58cc48e16c4c9fa80
Jason Pell <jason@pellcorp.com>
2024-02-09 21:07:47 +1100

    fixes

--- create.sh
+++ create.sh
@@ -26,7 +26,7 @@ rm -rf $DIR
 mkdir $DIR
 
 echo "Creating base in $DIR ..."
-debootstrap --foreign --arch mipsel --variant minbase bookworm $DIR http://ftp.au.debian.org/debian/ || exit $?
+debootstrap --foreign --arch mipsel bookworm $DIR http://ftp.au.debian.org/debian/ || exit $?
 
 echo "Finishing setup of base in $DIR ..."
 cp /usr/bin/qemu-mipsel-static $DIR/usr/bin
@@ -43,15 +43,12 @@ chroot "$DIR" apt-get install -y --no-install-recommends locales
 sed -i '/en_US.UTF-8/s/^# //;/en_US.UTF-8/s/^# //' "$DIR"/etc/locale.gen
 chroot "$DIR" /usr/sbin/locale-gen
 echo "LANG=en_US.UTF-8\nLANGUAGE=en_US.UTF-8" > "$DIR"/etc/default/locale
-chroot "$DIR" apt-get install -y --no-install-recommends openssh-server adduser file squashfs-tools
+chroot "$DIR" apt-get install -y --no-install-recommends openssh-server squashfs-tools
 
 echo "Installing kernel ..."
 chroot "$DIR" apt-get install -y --no-install-recommends linux-image-4kc-malta
 
-echo "Setting up debian user ..."
-chroot "$DIR" /usr/sbin/adduser --gecos "Debian user,,," --disabled-password debian
 echo "root:root" | chroot "$DIR" /usr/sbin/chpasswd
-echo "debian:debian" | chroot "$DIR" /usr/sbin/chpasswd
 
 echo "Setting up ssh ..."
 ln -s /dev/null "$DIR"/etc/systemd/network/99-default.link
@@ -62,10 +59,7 @@ cp keys/ssh_host_* "$DIR"/etc/ssh
 chmod 600 "$DIR"/etc/ssh/ssh_host_*_key
 echo 'PermitRootLogin yes' > "$DIR"/etc/ssh/sshd_config.d/permit_root.conf
 mkdir -p "$DIR"/root/.ssh
-mkdir -p "$DIR"/home/debian/.ssh
 cat keys/ssh_user_*.pub > "$DIR"/root/.ssh/authorized_keys
-cat keys/ssh_user_*.pub > "$DIR"/home/debian/.ssh/authorized_keys
-chroot "$DIR" chown -Rc debian:debian /home/debian/.ssh
 cp keys/ssh_user_*_key "$DIR"
 
 ln -L "$DIR"/vmlinux "$DIR"/kernel
