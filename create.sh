b111df6531de35d22a0f6952c9af300f65829d9f
Jason Pell <jason@pellcorp.com>
2024-02-09 20:55:04 +1100

    various fixes to the create.sh scrtipt to automate creation of the qemu artifacts

--- create.sh
+++ create.sh
@@ -1,36 +1,59 @@
-sudo rm -rf ~/debinst
-mkdir ~/debinst
-sudo debootstrap --foreign --arch mipsel bookworm ~/debinst http://ftp.au.debian.org/debian/
+#!/bin/bash
+
 # sudo pacman -S qemu-user-static
-sudo cp /usr/bin/qemu-mipsel-static ~/debinst/usr/bin
-sudo LC_CTYPE=en_US.UTF-8 LC_ALL=en_US.UTF-8 chroot ~/debinst/ qemu-mipsel-static /debootstrap/debootstrap --second-stage
 
-ARCH="mipsel"
-LINUX="linux-image-4kc-malta"
-QEMU_ARCH="mipsel"
-QEMU_MACHINE="malta"
-QEMU_DISK="-drive file=image.qcow2"
-QEMU_CPU="4KEc"
-QEMU_NET_DEVICE="-device e1000,netdev=net"
-CONSOLE="ttyS0"
+if [ "$(whoami)" != "root" ]; then
+  echo "Must be run as root"
+  exit 1
+fi
 
-    
-DIR=~/debinst
+LANG=en_US.UTF-8
+LANGUAGE=en_US.UTF-8
+LC_ALL=en_US.UTF-8
+unset LC_PAPER
+unset LC_NUMERIC
+unset LC_IDENTIFICATION
+unset LC_MEASUREMENT
+unset LC_NAME
+unset LC_TELEPHONE
+unset LC_ADDRESS
+unset LC_MONETARY
+unset LC_TIME
+
+DIR=/home/jason/debinst
+
+rm -rf $DIR
+mkdir $DIR
+
+echo "Creating base in $DIR ..."
+debootstrap --foreign --arch mipsel --variant minbase bookworm $DIR http://ftp.au.debian.org/debian/ || exit $?
+
+echo "Finishing setup of base in $DIR ..."
+cp /usr/bin/qemu-mipsel-static $DIR/usr/bin
+chroot $DIR/ /debootstrap/debootstrap --second-stage
+
+echo "Installing additional packages ..."
+
 # Install a simple fstab and set hostname
 cp fstab "$DIR"/etc/fstab
 echo "debian" > "$DIR"/etc/hostname
 
 chroot "$DIR" apt-get update
-chroot "$DIR" bash -c 'cat /var/lib/apt/lists/*_Packages  | grep '\''^\(Package\|Priority\): '\'' | grep -B 1 '\''^Priority: important'\'' | grep ^Package | cut -d'\'' '\'' -f2 | grep -v ^vim | grep -v ^isc-dhcp | xargs apt-get install -y --no-install-recommends'
-chroot "$DIR" apt-get install -y --no-install-recommends openssh-server adduser update-initramfs file locales linux-image-4kc-malta squashfs-tools
-chroot "$DIR" bash -c 'echo "LANG=en_US.UTF-8" > /etc/default/locale'
-chroot "$DIR" locale-gen
+chroot "$DIR" apt-get install -y --no-install-recommends locales
+sed -i '/en_US.UTF-8/s/^# //;/en_US.UTF-8/s/^# //' "$DIR"/etc/locale.gen
+chroot "$DIR" /usr/sbin/locale-gen
+echo "LANG=en_US.UTF-8\nLANGUAGE=en_US.UTF-8" > "$DIR"/etc/default/locale
+chroot "$DIR" apt-get install -y --no-install-recommends openssh-server adduser file squashfs-tools
 
-# Create and set passwords for root and user debian
+echo "Installing kernel ..."
+chroot "$DIR" apt-get install -y --no-install-recommends linux-image-4kc-malta
+
+echo "Setting up debian user ..."
 chroot "$DIR" /usr/sbin/adduser --gecos "Debian user,,," --disabled-password debian
 echo "root:root" | chroot "$DIR" /usr/sbin/chpasswd
 echo "debian:debian" | chroot "$DIR" /usr/sbin/chpasswd
 
+echo "Setting up ssh ..."
 ln -s /dev/null "$DIR"/etc/systemd/network/99-default.link
 cp interfaces "$DIR"/etc/network/interfaces
 cp resolv.conf "$DIR"/etc
@@ -45,16 +68,10 @@ cat keys/ssh_user_*.pub > "$DIR"/home/debian/.ssh/authorized_keys
 chroot "$DIR" chown -Rc debian:debian /home/debian/.ssh
 cp keys/ssh_user_*_key "$DIR"
 
-chroot "$DIR" /usr/sbin/update-initramfs -k all -c
-
-TARDISK="$DIR/image.tar.gz"
-DISK="$DIR/image.qcow2"
-
-LINUX_FILENAME="vmlinux"
-INITRD_FILENAME="initrd.img"
-
 ln -L "$DIR"/vmlinux "$DIR"/kernel
 ln -L "$DIR"/initrd.img "$DIR"/initrd
+
+echo "Creating qemu.tar.gz ..."
 
 virt-make-fs --format=qcow2 --size=10G --partition=gpt --type=ext4 --label=rootfs $DIR/ image.qcow2
 qemu-img convert -f qcow2 image.qcow2 -O qcow2 image2.qcow2
--- readme.md
+++ readme.md
@@ -14,31 +14,13 @@ Found this project which builds new debian images of various things:
 
 https://gitlab.com/giomasce/dqib
 
-Unfortunately I am having some issues running it locally, as mipsel is no longer supported for debian-unstable, and the debian-stable suite seems
-to stall out on the `chroot "$DIR"/chroot apt-get update` step.
-
 Anyway, I found the very last mipsel build made which is available here:
 https://gitlab.com/giomasce/dqib/-/jobs/4960385909
 
 Useful info from here:
 https://gitlab.com/giomasce/dqib/-/blob/d64002db448f4f3012183841cddbded6aed670d9/.gitlab-ci.yml
 
-# Running the qemu image
-
-## Network
-
-#```
-#sudo brctl addbr virbr0
-#sudo ifconfig virbr0 192.168.5.1/24 up
-# sudo ip tuntap del dev tap0 mode tap
-#sudo ip tuntap add dev tap0 mode tap dev tap0
-#sudo ifconfig tap0 192.168.5.11/24 up
-#sudo brctl addif virbr0 tap0
-#```
-
 ## Running it
-
-#qemu-system-mips -M malta -kernel kernel -hda image.qcow2 -append "root=/dev/sda1" -netdev tap,id=tapnet,ifname=tap0,script=no -device rtl8139,netdev=tapnet -nographic
 
 qemu-system-mipsel -machine malta -cpu 4KEc -m 1G -drive file=image.qcow2 -device e1000,netdev=net -netdev user,id=net,hostfwd=tcp::2222-:22 -kernel kernel -initrd initrd -nographic -append "root=LABEL=rootfs console=ttyS0"
 
@@ -56,3 +38,4 @@ mount -o bind /sys /root/squashfs/sys
 mount -t devpts none /root/squashfs/dev/pts
 
 chroot squashfs/ /bin/ash
+
