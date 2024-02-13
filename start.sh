da54acb707db3ce995fad404e6d5f42bf9360da2
Jason Pell <jason@pellcorp.com>
2024-02-13 13:43:30 +1100

    ooops the XBurstR2 is not working for me, I guess I need a specific kernel for it

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo chown $USER image.qcow2 initrd.img vmlinux
-qemu-system-mipsel -machine malta -cpu XBurstR2 -m 256 -smp cores=2 -drive file=image.qcow2 -device e1000,netdev=net -netdev user,id=net,hostfwd=tcp::2222-:22 -kernel vmlinux -initrd initrd.img -nographic -append "root=LABEL=rootfs console=ttyS0"
+qemu-system-mipsel -machine malta -cpu 4KEc -m 256 -smp cores=2 -drive file=image.qcow2 -device e1000,netdev=net -netdev user,id=net,hostfwd=tcp::2222-:22 -kernel vmlinux -initrd initrd.img -nographic -append "root=LABEL=rootfs console=ttyS0"
 
