59222d1fcc82c58992dbfe9203a96fa8f84f6dc0
Jason Pell <jason@pellcorp.com>
2024-05-25 16:03:14 +1000

    add port forwards for moonraker and mainsail

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ sudo chown $USER image.qcow2 initrd.img vmlinux
 # FIXME - I am not sure how to setup tab based networking :-(
 # so just forward a bunch of the ports we need
 NET_DEV="-device e1000,netdev=net"
-NET_DEVICE="-netdev user,id=net,hostfwd=tcp::2222-:22,hostfwd=tcp::9999-:9999,hostfwd=tcp::4408-:4408,hostfwd=tcp::4407-:4407,hostfwd=tcp::8080-:80"
+NET_DEVICE="-netdev user,id=net,hostfwd=tcp::2222-:22,hostfwd=tcp::9999-:9999,hostfwd=tcp::4408-:4408,hostfwd=tcp::4409-:4409,hostfwd=tcp::8080-:80,hostfwd=tcp::7125-:7125"
 
 # so far only -cpu 4KEc has worked for chroot of k1
 sudo qemu-system-mipsel -machine malta -cpu 4KEc -m 2G -drive file=image.qcow2 -kernel vmlinux -initrd initrd.img $NET_DEV $NET_DEVICE -nographic -append "root=LABEL=rootfs console=ttyS0"
