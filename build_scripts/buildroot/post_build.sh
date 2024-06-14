4e9260ae2110cf342fec31f8a59c1e7f1b29e1ce
Jason Pell <jason@pellcorp.com>
2024-06-14 22:27:18 +1000

    get an nginx build

--- build_scripts/buildroot/post_build.sh
+++ build_scripts/buildroot/post_build.sh
@@ -8,13 +8,31 @@ source $BASE_DIR/.mod_env
 export VARIANT_BUILDROOT_OUT=$BUILDROOT_OUT/$MOD_VARIANT
 
 mkdir -p ${TARGET_DIR}/usr/share/
-tar -xf $GIT_ROOT/prebuilt/klippy-env.tar.xz -C ${TARGET_DIR}/usr/share/
+tar -zxf $GIT_ROOT/prebuilt/klippy-env.tar.gz -C ${TARGET_DIR}/usr/share/
+# the rest of the env will hopefully be binary free
+cp ${TARGET_DIR}/usr/bin/python3 ${TARGET_DIR}/usr/share/klippy-env/bin/
 
 if [ "$MOD_VARIANT" == "x86_64" ]; then
-    echo "TARGET DIR: ${TARGET_DIR}"
     # this is as dodgy af but it works if you run it from a x86_64 build machine!
     ${TARGET_DIR}/usr/bin/python3 -m pip install --ignore-installed virtualenv==15.1.0 --no-deps --upgrade
-
-    # the rest of the env will hopefully be binary free
-    cp ${TARGET_DIR}/usr/bin/python3 ${TARGET_DIR}/usr/share/klippy-env/bin/
 fi
+
+# I am sure there is a better way, but creality packages nginx separately
+# so for testing purposes we also need it separately
+[ -f $VARIANT_BUILDROOT_OUT/images/nginx-$MOD_VARIANT.tar.gz ] && rm $VARIANT_BUILDROOT_OUT/images/nginx-$MOD_VARIANT.tar.gz
+mkdir -p /tmp/nginx.$$/nginx/sbin
+mkdir -p /tmp/nginx.$$/nginx/nginx
+mkdir -p /tmp/nginx.$$/nginx/logrotate.d
+
+# we need to redirect the nginx package separately
+cp ${TARGET_DIR}/usr/sbin/nginx /tmp/nginx.$$/nginx/sbin/
+cp ${TARGET_DIR}/etc/nginx/* /tmp/nginx.$$/nginx/nginx/
+cp ${TARGET_DIR}/etc/logrotate.d/nginx /tmp/nginx.$$/nginx/logrotate.d/
+#rm -rf ${TARGET_DIR}/usr/sbin/nginx
+rm -rf ${TARGET_DIR}/etc/nginx
+rm ${TARGET_DIR}/etc/init.d/S50nginx
+
+pushd /tmp/nginx.$$ > /dev/null
+tar -zcf $VARIANT_BUILDROOT_OUT/images/nginx-$MOD_VARIANT.tar.gz nginx/
+popd > /dev/null
+rm -rf /tmp/nginx.$$ 
