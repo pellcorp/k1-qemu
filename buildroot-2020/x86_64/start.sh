ce9b5c11436031f90ad6f5fb38c638f0e09e048b
Jason Pell <jason@pellcorp.com>
2024-06-14 14:37:13 +1000

    add some port forwarding

--- buildroot-2020/x86_64/start.sh
+++ buildroot-2020/x86_64/start.sh
@@ -3,4 +3,6 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P)"
 cd $CURRENT_DIR
 
-qemu-system-x86_64 -M pc -kernel bzImage -drive file=rootfs.ext2,if=virtio,format=raw -append "rootwait root=/dev/vda console=tty1 console=ttyS0" -net nic,model=virtio -net user -nographic
+NET_USER="user,id=net,hostfwd=tcp::2222-:22,hostfwd=tcp::9999-:9999,hostfwd=tcp::4408-:4408,hostfwd=tcp::4409-:4409,hostfwd=tcp::9080-:80,hostfwd=tcp::7125-:7125"
+
+qemu-system-x86_64 -M pc -kernel bzImage -drive file=rootfs.ext2,if=virtio,format=raw -append "rootwait root=/dev/vda console=tty1 console=ttyS0" -net nic,model=virtio -net $NET_USER -nographic
