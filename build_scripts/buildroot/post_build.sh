c18a9c7a66cd69e361605be7c77822ed288f9531
Jason Pell <jason@pellcorp.com>
2024-06-14 21:20:19 +1000

    add klippy-env with x86 support

--- build_scripts/buildroot/post_build.sh
+++ build_scripts/buildroot/post_build.sh
@@ -7,8 +7,14 @@ source $BASE_DIR/.mod_env
 
 export VARIANT_BUILDROOT_OUT=$BUILDROOT_OUT/$MOD_VARIANT
 
+mkdir -p ${TARGET_DIR}/usr/share/
+tar -xf $GIT_ROOT/prebuilt/klippy-env.tar.xz -C ${TARGET_DIR}/usr/share/
+
 if [ "$MOD_VARIANT" == "x86_64" ]; then
     echo "TARGET DIR: ${TARGET_DIR}"
     # this is as dodgy af but it works if you run it from a x86_64 build machine!
     ${TARGET_DIR}/usr/bin/python3 -m pip install --ignore-installed virtualenv==15.1.0 --no-deps --upgrade
+
+    # the rest of the env will hopefully be binary free
+    cp ${TARGET_DIR}/usr/bin/python3 ${TARGET_DIR}/usr/share/klippy-env/bin/
 fi
