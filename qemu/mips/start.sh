7c1a5878af83433d6c7caeb74d1e88d20d52fee8
Jason Pell <jason@pellcorp.com>
2024-06-14 18:59:33 +1000

    don't need these as we already have them under build scripts

--- qemu/mips/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P)"
-
-cd $CURRENT_DIR
-qemu-system-mipsel -M malta -kernel vmlinux  -drive file=rootfs.ext2,format=raw -append "rootwait root=/dev/hda" -net nic,model=pcnet -net user -nographic
