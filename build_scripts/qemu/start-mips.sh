5fa4f3225c9da236f4c141aa382db8b6eefe3552
Jason Pell <jason@pellcorp.com>
2024-06-16 17:37:32 +1000

    more ram, add ext2 and wpa_supplicant

--- build_scripts/qemu/start-mips.sh
+++ build_scripts/qemu/start-mips.sh
@@ -5,4 +5,4 @@ cd $CURRENT_DIR
 
 NET_USER="user,id=net,hostfwd=tcp::2222-:22,hostfwd=tcp::9999-:9999,hostfwd=tcp::4408-:4408,hostfwd=tcp::4409-:4409,hostfwd=tcp::9080-:80,hostfwd=tcp::7125-:7125"
 
-qemu-system-mipsel -M malta -kernel vmlinux  -drive file=rootfs.ext2,format=raw -append "rootwait root=/dev/hda" -net nic,model=pcnet -net $NET_USER -nographic
+qemu-system-mipsel -M malta -m 2G -kernel vmlinux -drive file=rootfs.ext2,format=raw -append "rootwait root=/dev/hda" -net nic,model=pcnet -net $NET_USER -nographic
--- build_scripts/qemu/start-x86_64.sh
+++ build_scripts/qemu/start-x86_64.sh
@@ -5,4 +5,4 @@ cd $CURRENT_DIR
 
 NET_USER="user,id=net,hostfwd=tcp::2222-:22,hostfwd=tcp::9999-:9999,hostfwd=tcp::4408-:4408,hostfwd=tcp::4409-:4409,hostfwd=tcp::9080-:80,hostfwd=tcp::7125-:7125"
 
-qemu-system-x86_64 -M pc -kernel bzImage -drive file=rootfs.ext2,if=virtio,format=raw -append "rootwait root=/dev/vda console=tty1 console=ttyS0" -net nic,model=virtio -net $NET_USER -nographic
+qemu-system-x86_64 -M pc -m 2G -smp cpus=4 -kernel bzImage -drive file=rootfs.ext2,if=virtio,format=raw -append "rootwait root=/dev/vda console=tty1 console=ttyS0" -net nic,model=virtio -net $NET_USER -nographic
