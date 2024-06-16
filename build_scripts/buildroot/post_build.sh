f4c2f82fedccf6140c32afe5f2bdad2bd9399cef
Jason Pell <jason@pellcorp.com>
2024-06-16 13:27:39 +1000

    forget about mips its as slow as old mips, focusing on x86_64 now and just do a few little hacks for basic simple af installer testing

--- build_scripts/buildroot/post_build.sh
+++ build_scripts/buildroot/post_build.sh
@@ -17,27 +17,29 @@ if [ "$MOD_VARIANT" == "x86_64" ]; then
     ${TARGET_DIR}/usr/bin/python3 -m pip install --ignore-installed virtualenv==15.1.0 --no-deps --upgrade
 fi
 
-# if [ -f ${TARGET_DIR}/usr/sbin/nginx ]; then
-#     # I am sure there is a better way, but creality packages nginx separately
-#     # so for testing purposes we also need it separately
-#     [ -f $VARIANT_BUILDROOT_OUT/images/nginx-$MOD_VARIANT.tar.gz ] && rm $VARIANT_BUILDROOT_OUT/images/nginx-$MOD_VARIANT.tar.gz
+if [ -f ${TARGET_DIR}/usr/sbin/nginx ]; then
+    # I am sure there is a better way, but creality packages nginx separately
+    # so for testing purposes we also need it separately
+    [ -f $VARIANT_BUILDROOT_OUT/images/nginx-$MOD_VARIANT.tar.gz ] && rm $VARIANT_BUILDROOT_OUT/images/nginx-$MOD_VARIANT.tar.gz
 
-#     mkdir -p /tmp/nginx.$$/nginx/sbin
-#     mkdir -p /tmp/nginx.$$/nginx/nginx
-#     mkdir -p /tmp/nginx.$$/nginx/logrotate.d
+    mkdir -p /tmp/nginx.$$/nginx/sbin
+    mkdir -p /tmp/nginx.$$/nginx/nginx
+    mkdir -p /tmp/nginx.$$/nginx/logrotate.d
 
-#     # we need to redirect the nginx package separately
-#     cp ${TARGET_DIR}/usr/sbin/nginx /tmp/nginx.$$/nginx/sbin/
-#     cp ${TARGET_DIR}/etc/nginx/* /tmp/nginx.$$/nginx/nginx/
-#     cp ${TARGET_DIR}/etc/logrotate.d/nginx /tmp/nginx.$$/nginx/logrotate.d/
-#     #rm -rf ${TARGET_DIR}/usr/sbin/nginx
-#     rm -rf ${TARGET_DIR}/etc/nginx
-#     rm ${TARGET_DIR}/etc/init.d/S50nginx
+    # we need to redirect the nginx package separately
+    cp ${TARGET_DIR}/usr/sbin/nginx /tmp/nginx.$$/nginx/sbin/
+    cp ${TARGET_DIR}/etc/nginx/* /tmp/nginx.$$/nginx/nginx/
+    cp ${TARGET_DIR}/etc/logrotate.d/nginx /tmp/nginx.$$/nginx/logrotate.d/
+    #rm -rf ${TARGET_DIR}/usr/sbin/nginx
+    #rm -rf ${TARGET_DIR}/etc/nginx
+    if [ -f ${TARGET_DIR}/etc/init.d/S50nginx ]; then
+        mv ${TARGET_DIR}/etc/init.d/S50nginx ${TARGET_DIR}/etc/init.d/X50nginx 
+    fi
 
-#     pushd /tmp/nginx.$$ > /dev/null
-#     tar -zcf $VARIANT_BUILDROOT_OUT/images/nginx-$MOD_VARIANT.tar.gz nginx/
-#     popd > /dev/null
-#     rm -rf /tmp/nginx.$$ 
-# else
-#     echo "WARNING: Skipped nginx"
-# fi
+    pushd /tmp/nginx.$$ > /dev/null
+    tar -zcf $VARIANT_BUILDROOT_OUT/images/nginx-$MOD_VARIANT.tar.gz nginx/
+    popd > /dev/null
+    rm -rf /tmp/nginx.$$ 
+else
+    echo "WARNING: Skipped nginx"
+fi
