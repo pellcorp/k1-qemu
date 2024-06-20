7d39e219f75643376ed9ad08378bd4a561d13731
Jason Pell <jason@pellcorp.com>
2024-06-20 20:55:58 +1000

    add v4l ctl

--- build_scripts/buildroot/post_build.sh
+++ build_scripts/buildroot/post_build.sh
@@ -7,9 +7,12 @@ source $BASE_DIR/.mod_env
 
 export VARIANT_BUILDROOT_OUT=$BUILDROOT_OUT/$MOD_VARIANT
 
-if [ "$MOD_VARIANT" != "nginx" ]; then
+if [ "$MOD_VARIANT" = "x86_64" ]; then
     mkdir -p ${TARGET_DIR}/usr/share/
     tar -zxf $GIT_ROOT/prebuilt/klippy-env.tar.gz -C ${TARGET_DIR}/usr/share/
     # the rest of the env will hopefully be binary free
     cp ${TARGET_DIR}/usr/bin/python3 ${TARGET_DIR}/usr/share/klippy-env/bin/
+
+    # dont start nginx but leave the bin file
+    [ -f ${TARGET_DIR}/etc/init.d/S50nginx ] && rm ${TARGET_DIR}/etc/init.d/S50nginx
 fi
