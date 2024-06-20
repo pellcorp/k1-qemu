e5146336757d7380263956f2f1daf9e09e419067
Jason Pell <jason@pellcorp.com>
2024-06-20 19:46:54 +1000

    remove nginx and dodgy stuff

--- build_scripts/buildroot/post_build.sh
+++ build_scripts/buildroot/post_build.sh
@@ -12,42 +12,4 @@ if [ "$MOD_VARIANT" != "nginx" ]; then
     tar -zxf $GIT_ROOT/prebuilt/klippy-env.tar.gz -C ${TARGET_DIR}/usr/share/
     # the rest of the env will hopefully be binary free
     cp ${TARGET_DIR}/usr/bin/python3 ${TARGET_DIR}/usr/share/klippy-env/bin/
-
-    if [ "$MOD_VARIANT" == "x86_64" ]; then
-        # this is as dodgy af but it works if you run it from a x86_64 build machine!
-        ${TARGET_DIR}/usr/bin/python3 -m pip install --ignore-installed virtualenv==15.1.0 --no-deps --upgrade
-    fi
-
-    if [ -f ${TARGET_DIR}/usr/sbin/nginx ]; then
-        # I am sure there is a better way, but creality packages nginx separately
-        # so for testing purposes we also need it separately
-        [ -f $VARIANT_BUILDROOT_OUT/images/nginx-$MOD_VARIANT.tar.gz ] && rm $VARIANT_BUILDROOT_OUT/images/nginx-$MOD_VARIANT.tar.gz
-
-        mkdir -p /tmp/nginx.$$/nginx/sbin
-        mkdir -p /tmp/nginx.$$/nginx/nginx
-        mkdir -p /tmp/nginx.$$/nginx/logrotate.d
-
-        # we need to redirect the nginx package separately
-        cp ${TARGET_DIR}/usr/sbin/nginx /tmp/nginx.$$/nginx/sbin/
-        cp ${TARGET_DIR}/etc/nginx/* /tmp/nginx.$$/nginx/nginx/
-        cp ${TARGET_DIR}/etc/logrotate.d/nginx /tmp/nginx.$$/nginx/logrotate.d/
-        #rm -rf ${TARGET_DIR}/usr/sbin/nginx
-        #rm -rf ${TARGET_DIR}/etc/nginx
-        if [ -f ${TARGET_DIR}/etc/init.d/S50nginx ]; then
-            mv ${TARGET_DIR}/etc/init.d/S50nginx ${TARGET_DIR}/etc/init.d/X50nginx 
-        fi
-
-        pushd /tmp/nginx.$$ > /dev/null
-        tar -zcf $VARIANT_BUILDROOT_OUT/images/nginx-$MOD_VARIANT.tar.gz nginx/
-        popd > /dev/null
-        rm -rf /tmp/nginx.$$ 
-    else
-        echo "WARNING: Skipped nginx"
-    fi
- else
-     interpreter=$($VARIANT_BUILDROOT_OUT/host/usr/bin/patchelf --print-interpreter ${TARGET_DIR}/usr/sbin/nginx)
-     if [[ $interpreter != /opt* ]]; then
-         echo "Fixing interpreter and rpath ..."
-        /usr/bin/patchelf --set-interpreter /opt${interpreter} --set-rpath '/opt/lib:/opt/usr/lib' ${TARGET_DIR}/usr/sbin/nginx
-     fi
 fi
